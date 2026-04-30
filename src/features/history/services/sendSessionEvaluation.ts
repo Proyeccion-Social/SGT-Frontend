ef6029e92d9f162d712f47bfa2431eac68d1a9ab
devCristian <criscamcr7@gmail.com>
2026-04-29 19:02:36 -0500

    update: Styles for new dasboard. Partial final styles for history

--- src/features/history/services/sendSessionEvaluation.ts
+++ src/features/history/services/sendSessionEvaluation.ts
@@ -1,5 +1,4 @@
 const API_URL = import.meta.env.API_URL;
-import { mockSendEvaluation } from "@/features/history/mocks/sendEvaluation";
 
 export interface EvaluationPayload {
   ratings: {
@@ -11,19 +10,12 @@ export interface EvaluationPayload {
   overallRating?: number;
   comments?: string;
 }
-const USE_MOCK = false;
     
 export async function sendSessionEvaluation(
   sessionId: string,
   payload: EvaluationPayload,
   token?: string
 ) {
-  if (USE_MOCK) {
-    //  simulamos delay real
-    await new Promise((res) => setTimeout(res, 800));
-
-    return mockSendEvaluation(sessionId, payload);
-  }
 
   const response = await fetch(
     `${API_URL}/session-execution/sessions/${sessionId}/evaluation`,

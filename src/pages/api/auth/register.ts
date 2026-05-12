449340561c60e955d40cf6e6643f45da2a3e3ff9
devCristian <criscamcr7@gmail.com>
2026-05-11 22:00:43 -0500

    Revert: register.ts2

--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -9,27 +9,15 @@ export const POST: APIRoute = async ({ request, cookies }) => {
 		const result = await register(data);
 
 		return new Response(
-			JSON.stringify({ user: result.user }),
+			JSON.stringify({ user: result.user }),	
 			{
 				status: 200,
 				headers: { "Content-Type": "application/json" }
 			}
 		);
-<<<<<<< HEAD
-<<<<<<< HEAD
-	} catch (error: any) {
-		return new Response(
-			JSON.stringify({ message: getErrorMessage(error.message) }),
-=======
 	} catch (error: any) {  
 		return new Response(
 			JSON.stringify({ message: error.message }),
->>>>>>> parent of d3b9c89 (fix: drawe ya solo se abre en confirm email)
-=======
-	} catch (error: any) {  
-		return new Response(
-			JSON.stringify({ message: error.message }),
->>>>>>> parent of 98ad86a (feat: mensajes de error personalizados según el tipo de fallo del backend)
 			{ status: 400 }
 		);
 	}
